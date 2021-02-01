42e73fa3bad07951c6db75b3aeecc50c4c612319
Do <whqtkf12@naver.com>
2021-02-01 21:50:16 +0900

    list sort lambda

--- sort.py
+++ sort.py
@@ -11,11 +11,11 @@ for i in range(len(array)-1):
 
 print(array)
 
-#2. insertion sort: 데이터가 정렬이 잘되있을수록 빠르게 동작함
+#2. insrtion sort: 데이터가 정렬이 잘되있을수록 빠르게 동작함
 array=[7,5,9,0,3,1,6,2,4,8]
 for i in range(1,len(array)):
     for j in range(i,0,-1):
-        if array[j]<array[j-1]:
+        if array[j]<array[j-1]
             array[j],array[j-1]=array[j-1],array[j]
         else:
             break
@@ -68,4 +68,17 @@ for i in range(len(array)):
 
 for i in range(len(count)):
     for j in range(count[i]):
-        print(i,end=' ')
+       print(i,end=' ')
+
+#python의 정렬 라이브러리
+#sorted() 함수. 퀵정렬보단 느리지만 최악의 경우에도 NlogN보장 input: list,dic,set... output: list
+#array.sort(). 별도의 리스트를 반환하지 않고 리스트 자체가 바뀐다
+#key 매개변수를 입력으로 받을 수 있다.
+
+#아래와같이하면 2차원배열 두값을 기준으로 정렬가능. - 붙이면 거꾸로. 앞에있는게 우선순위 높음
+array.sort(key=lambda x: (x[0],x[1]))
+
+
+
+
+
